7967c43c6471bfc8a0515f62fd8bb5e6471c404f
bretThinkful <bretbickhamx1989@gmail.com>
2020-12-06 12:23:07 -0700

    Update bookmarkList.js

--- scripts/bookmarkList.js
+++ scripts/bookmarkList.js
@@ -5,7 +5,7 @@ const bookmarkList = (function(){
   function generateBookmarkElement(item) {
     return `
     <li class="bookmark-list-items js-bookmark-list-items" data-item-id="${item.id}">
-      <h2 class="list-title js-list-title">${item.title}</h2>
+      <h3 class="list-title js-list-title">${item.title}</h3>
       <section class="star-rating js-star-rating">
         <p class="star-number js-star-number">${item.rating} Stars</p>
       </section>
