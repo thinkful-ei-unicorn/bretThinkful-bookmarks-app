e9ed052a1a750ab06cda31c64adb7dae84bf06a7
bretThinkful <bretbickhamx1989@gmail.com>
2020-12-09 15:57:44 -0700

    Update store.js

--- scripts/store.js
+++ scripts/store.js
@@ -1,41 +1,30 @@
 'use strict';
-
 const store =(function(){
-
   const addBookmarkItem = function(item) {
     this.items.push(item);
+    this.allitems.push( item );
   };
-    
-  
   const findItemById = function(id) {
-    return this.items.find(item => item.id === id);
+    return this.allitems.find(item => item.id === id);
   };
-    
-
   const findAndDelete = function(id) {
     this.items = this.items.filter(item => item.id !== id);
+    this.allitems = this.allitems.filter(item => item.id !== id);
   };
-    
-    
   function filterByRating(val) {
-    this.items = this.items.filter(item => {
+    this.items = this.allitems.filter(item => {
       return item.rating >= val;
     });
   }
-
-  
-
   const setItemEditing = function (id, isEditing) {
     const item = this.findById(id);
     item.isEditing = !item.isEditing;
   };
-
-
   return {
     items: [],
+    allitems : [],
     adding: false,
     error: null,
-    
     addItem: addBookmarkItem,
     findById: findItemById,
     findAndDelete, 
